64d1527ac3af89850df2adf595afa2973f1b5819
SamiaBenNasr <samia.bennasr@insat.ucar.tn>
2026-07-04 15:09:46 +0100

    adding yml files for Azure ML deployment

--- test.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-root = "data/mvtec/bottle"
-
-print("Train good:", len(list(Path(root + "/train/good").glob("*"))))
-print("Test good:", len(list(Path(root + "/test/good").glob("*"))))
